11e63eee57af45ed30fffa2b830227b32ac31bac
faysor <faysorx3@gmail.com>
2020-05-01 00:59:33 -0400

    feat: solo permitir cerrar sesión cuando han iniciado sesión

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 Route::get('/home', 'HomeController@index')->name('home');
 Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout');
-*/
+Route::post('logout', 'Auth\LoginController@logout')->middleware('auth:sanctum');
